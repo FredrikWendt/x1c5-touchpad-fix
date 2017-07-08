628401344b99cffed100f52b892fbf44201daa51
Fredrik Wendt <fredrik@wendt.se>
2017-07-08 12:47:16 +0200

    no-cache and don't overwrite kernel-config if provided

--- git-upstream/build.sh
+++ git-upstream/build.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-cp /boot/config-$(uname -r) kernel-config
+START=$(date -Iminutes)
 
-docker build -t git-upstream-kernel-build .
-curl -fsSL 'http://push.wendt.io/?message=kernel-build-ready' >> /dev/null
+echo "Build starting at $START"
+
+if [ ! -f kernel-config ] ; then
+	CONFIG=/boot/config-$(uname -r)
+	echo "Copying kernel build config from $CONFIG"
+	cp $CONFIG kernel-config
+else
+	echo "Using the kernel-config file already there"
+fi
+
+docker build --no-cache -t git-upstream-kernel-build .
+
+END=$(date -Iminutes)
+
+echo "Build completed: from $START to $END"
+
+cat <<EOF
+docker cp  these files from the container:
+linux-firmware-image-4.12.0-rc7-custom_4.12.0-rc7-custom-1_amd64.deb
+linux-headers-4.12.0-rc7-custom_4.12.0-rc7-custom-1_amd64.deb
+linux-image-4.12.0-rc7-custom_4.12.0-rc7-custom-1_amd64.deb
+linux-libc-dev_4.12.0-rc7-custom-1_amd64.deb
+EOF
